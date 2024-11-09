bd788f68b3c6554360381108187d5e0e869bcbea
Rahulkoju <blcraz75@gmail.com>
2024-11-09 22:41:35 +0545

    add: scraped data in nepali format

---
+++ app/Http/Controllers/ScrapController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ScrapController extends Controller
+{
+    public function scrap()
+    {
+        $url = 'https://kalimatimarket.gov.np/';
+        $res = Http::get($url);
+        $res = $res->body();
+        $productData = $this->extractProductData($res);
+        return response()->json($productData);
+    }
+
+    protected function extractProductData($res)
+    {
+        $productData = [];
+
+        $pattern = '/<tr>(.*?)<\/tr>/s';
+        preg_match_all($pattern, $res, $matches);
+        foreach ($matches[1] as $row) {
+            $product = $this->parseProductHtml($row);
+            $productData[] = $product;
+        }
+
+        return array_values(array_filter($productData, function ($item) {
+            return !empty($item) && array_filter($item);
+        }));
+
+        return $filteredData;
+    }
+
+    protected function parseProductHtml($row)
+    {
+        $prices = $this->extractPrices($row);
+
+        $product = [
+            'name' => $this->extractValue($row, '/class="dt-body-left">(.*?)<span/'),
+            'min' => $prices[0] ?? '',
+            'max' => $prices[1] ?? '',
+            'avg' => $prices[2] ?? '',
+        ];
+
+        return $product;
+    }
+
+    protected function extractValue($row, $pattern)
+    {
+        if (preg_match($pattern, $row, $matches)) {
+            return trim($matches[1]);
+        }
+    }
+
+    protected function extractPrices($row)
+    {
+        $pattern = '/रू\s*([\d]+)/u';
+        preg_match_all($pattern, $row, $matches);
+        return $matches[1] ?? [];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ScrapController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/scrap', [ScrapController::class, 'scrap']);
